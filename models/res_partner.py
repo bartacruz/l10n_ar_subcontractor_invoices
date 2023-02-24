b8151964af9bfc7b72e38384c4da5f7272026563
Julio Santa Cruz <bartacruz@gmail.com>
2023-02-24 02:15:00 -0300

    Partner views

--- models/res_partner.py
+++ models/res_partner.py
@@ -11,6 +11,18 @@ class ResPartner(models.Model):
 
     subcontractor_pos_number = fields.Integer(string=_('Subcontractor POS'),
                                             help=_('Point of sale used to post subcontractor invoices'))
-
-
+    subcontractor_invoice_ids = fields.One2many('account.move','subcontractor_partner_id',_('Subcontractor invoices'))
+    subcontractor_invoice_count = fields.Integer('Subcontractor invoice count', compute="_compute_subcontractor_invoice_count", store=True)
     
+    @api.depends('subcontractor_invoice_ids')
+    def _compute_subcontractor_invoice_count(self):
+        for record in self:
+            record.subcontractor_invoice_count = len(record.subcontractor_invoice_ids)
+    
+    def action_open_subcontractor_invoices(self):
+        self.ensure_one()
+        action = self.env.ref("l10n_ar_subcontractor_invoices.action_subcontractor_invoice_type").read()[0]
+        action["context"] = {}
+        action["domain"] = [("id", "in", self.subcontractor_invoice_ids.ids)]
+        
+        return action
